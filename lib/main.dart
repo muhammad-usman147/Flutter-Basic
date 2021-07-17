3fe47854b0df55b8386c0b362bd9f1fc34540cc1
Muhammad Usman Shakeel <ushakeel808@gmail.com>
2021-07-17 22:35:30 +0500

    'sample'

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'LoginUI.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: LoginUI(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
